1b072652a11ff81dfa580a53c633c1a817b54e4d
Daniel Molina <dmolina@decsai.ugr.es>
2021-04-01 20:31:09 +0200

    Add examples.

---
+++ codigo/test.cc
@@ -0,0 +1,27 @@
+extern "C" {
+#include "cec17.h"
+}
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  vector<double> sol;
+  int dim = 10;
+
+  for (int funcid = 1; funcid <= 30; funcid++) {
+    vector<double> sol(dim);
+    double fitness;
+
+    cec17_setfun(funcid, dim);
+
+    for (int i = 0; i < dim; i++) {
+      sol[i] = i;
+    }
+
+    fitness = cec17_fitness(&sol[0]);
+    cout <<"Fitness[F" <<funcid <<"]: " << scientific <<cec17_error(fitness) <<endl;
+  }
+
+}
---
+++ codigo/testrandom.cc
@@ -0,0 +1,43 @@
+extern "C" {
+#include "cec17.h"
+}
+#include <iostream>
+#include <vector>
+#include <random>
+
+using namespace std;
+
+int main() {
+  vector<double> sol;
+  int dim = 10;
+  int seed = 42;
+  std::uniform_real_distribution<> dis(-100.0, 100.0);
+
+  for (int funcid = 1; funcid <= 30; funcid++) {
+    vector<double> sol(dim);
+    vector<double> bestsol(dim);
+    double fitness;
+    double best = -1;
+
+    cec17_setfun(funcid, dim);
+    std::mt19937 gen(seed); // Inicio semilla
+    int evals = 0;
+
+    while (evals <= 10000*dim) {
+      for (int i = 0; i < dim; i++) {
+        sol[i] = dis(gen);
+      }
+
+      fitness = cec17_fitness(&sol[0]);
+      evals += 1;
+
+      if (evals == 1 || fitness < best) {
+        best = fitness;
+        bestsol = sol;
+      }
+    }
+
+    cout <<"Best Random[F" <<funcid <<"]: " << scientific <<cec17_error(best) <<endl;
+  }
+
+}
